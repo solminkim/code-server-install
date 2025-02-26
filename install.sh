fae60937e32f97ed1303d18f74e01a115fc3979e
solminkim <91516514+solminkim@users.noreply.github.com>
2025-02-26 09:28:25 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -80,7 +80,7 @@ echo_latest_version() {
     # https://gist.github.com/lukechilds/a83e1d7127b78fef38c2914c4ececc3c#gistcomment-2758860
     version="$(curl -fsSLI -o /dev/null -w "%{url_effective}" https://github.com/coder/code-server/releases/latest)"
   fi
-  version=v4.96.4
+  version=4.96.4
   #version="${version#v}"
   echo "$version"
 }
